ad1389484f6f17749f7d7e3c514575914d66445c
Guillaume Philippon <guillaume.philippon@olympe-hn.org>
2022-09-08 05:55:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ print("Ceci est un test")
 def MaFonction():
   print("Ceci est une fonction")
 MaFonction()
+
+import os
+import sys
+
+FileName = sys.argv[1]
+
+f = open(FileName, 'r')
+print(FileName)
